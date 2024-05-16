a0b0326e7e6a2010e8372d0a50f7dfd9e845d6fc
ritheecreator <ritheecreator@gmail.com>
2024-05-16 11:47:35 -0400

    responsive

--- web/server.js
+++ web/server.js
@@ -558,3 +558,14 @@ app.get('/get-billing-info', (req, res) => {
         res.status(404).send('No billing information found');
     }
 });
+
+app.get('/order/:InvoiceID', (req, res) => {
+    const invoiceID = req.params.invoiceID;
+    const query = `SELECT * FROM Invoice WHERE InvoiceID = ${invoiceID}`;
+  
+    connection.query(query, (error, results, fields) => {
+      if (error) throw error;
+      const invoice = results[0]; // Assuming only one invoice per order for simplicity
+      res.render('order', { invoice });
+    });
+  });
